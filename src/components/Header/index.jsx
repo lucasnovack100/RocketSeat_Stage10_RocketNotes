16b2c731bb0e3d3cb6a50e1e6ebf08e828e24e40
lucasnovack100 <lucasnovack@gmail.com>
2023-05-26 15:16:09 -0300

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import { Container, Profile, Logout } from "./styles";
+import { RiShutDownLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+
+import { api } from "../../../../../10/Api/src/services/api";
+import  avatarPlaceholder  from "../../assets/avatar_placeholder.svg";
+
+import { useAuth } from "../../hooks/auth";
+
+export function Header(){
+    const { signOut, user } = useAuth();
+    const navigation = useNavigate();
+
+    function handleSignOut() {
+        navigation("/");
+        signOut();
+    }
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+    return(
+        <Container>
+            <Profile to="/profile">
+                <img src={avatarUrl} alt={user.name} />
+                <div>
+                    <span>Bem Vindo</span>
+                    <strong>{user.name}</strong>
+                </div>
+            </Profile>
+
+            <Logout onClick={handleSignOut}>
+                <RiShutDownLine/>
+            </Logout>
+        </Container>
+    );
+}
